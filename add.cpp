5e3e3d5807ad1e66e20efeb5093961ce90d7318b
j4kmine <cbzvdani@gmail.com>
2017-08-14 05:33:21 +0700

    versi 1.0.0

---
+++ add.cpp
@@ -0,0 +1,6 @@
+#include "add.h"
+
+add::add(QWidget *parent) : QWidget(parent)
+{
+
+}
---
+++ add.h
@@ -0,0 +1,17 @@
+#ifndef ADD_H
+#define ADD_H
+
+#include <QWidget>
+
+class add : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit add(QWidget *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // ADD_H
